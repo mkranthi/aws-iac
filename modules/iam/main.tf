9c78beca90626fa89f8ffa8a3f25972115243b6d
mkranthi <kranthikumar1210@gmail.com>
2024-11-19 21:30:54 +0530

    adding modules

---
+++ modules/iam/main.tf
@@ -0,0 +1,56 @@
+resource "aws_iam_role" "iam_role" {
+  name = var.role_name
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name = var.role_name
+  }
+}
+#creating  iam_policy for role
+resource "aws_iam_policy" "iam_policy" {
+    name = var.iam_policy
+    policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:ListBucket",
+          "s3:ListAllMyBuckets",
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:DeleteObject"
+    
+        ]
+        Effect   = "Allow"
+        Resource = [
+    "arn:aws:s3:::kranti-terraform-statefile",
+    "arn:aws:s3:::kranti-terraform-statefile/*"
+  ]
+      }
+    ]
+  })
+}
+#attaching policy and role 
+resource "aws_iam_policy_attachment" "policy_attachment" {
+    name = "${var.role_name}_attachment"
+    roles = [aws_iam_role.iam_role.name]
+    policy_arn = aws_iam_policy.iam_policy.arn
+  
+}
+#creating instance profile
+resource "aws_iam_instance_profile" "my_instance_profile" {
+    name = "${var.role_name}_instance_profile"
+    role = aws_iam_role.iam_role.name
+
+}
