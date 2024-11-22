9b0460bc754dc053b9d922a222f0d00ed2056da2
mkranthi <kranthikumar1210@gmail.com>
2024-11-22 15:39:45 +0530

    remove the comment

--- main.tf
+++ main.tf
@@ -2,13 +2,13 @@ provider "aws" {
   region = "us-east-2"
 }
 
-#terraform {
-#  backend "s3" {
-#    bucket = "kranti-terraform-statefile" 
-#    key    = "terraform/terraform.tfstate"  
-#    region = "us-east-2"  
-#}
-#}
+terraform {
+  backend "s3" {
+    bucket = "kranti-terraform-statefile" 
+    key    = "terraform/terraform.tfstate"  
+    region = "us-east-2"  
+  }
+}
 
 module "iam" {
   source = "./modules/iam"
