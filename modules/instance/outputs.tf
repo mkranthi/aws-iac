4ee39884db5663c113faa385a45c9d801518e259
mkranthi <kranthikumar1210@gmail.com>
2024-11-25 14:12:07 +0530

    Update outputs.tf

--- modules/instance/outputs.tf
+++ modules/instance/outputs.tf
@@ -8,11 +8,6 @@ output "instance_id" {
   value       = aws_instance.terraform_instance.id
 }
 
-output "iam_role_name" {
-  description = "The name of the IAM role"
-  value       = data.aws_iam_role.existing_role.name
-}
-
 output "instance_profile_name" {
   description = "The name of the IAM instance profile"
   value       = data.aws_iam_instance_profile.existing_instance_profile.name
