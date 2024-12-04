7e20a388aa53fc2da8309ed023ecca06b4996a7f
mkranthi <kranthikumar1210@gmail.com>
2024-12-04 21:11:03 +0530

    removed duplicate value

--- modules/iam/output.tf
+++ modules/iam/output.tf
@@ -1,19 +1,14 @@
 output "iam_instance_profile_name" {
-  value = aws_iam_instance_profile.my_instance_profile.name
-}
-
-output "iam_role_name" {
-  value = aws_iam_role.kms.name
-}
-output "kms_role_name" {
-  description = "The name of the KMS IAM role"
-  value       = aws_iam_role.kms.name
+  description = "The name of the IAM instance profile"
+  value       = aws_iam_instance_profile.my_instance_profile.name
 }
 
 output "role_name" {
-  value = aws_iam_role.iam_role.name
+  description = "The name of the primary IAM role"
+  value       = aws_iam_role.iam_role.name
 }
 
-output "kms_role" {
-  value = aws_iam_role.kms_admin_role.name
+output "kms_role_name" {
+  description = "The name of the KMS IAM role"
+  value       = aws_iam_role.kms.name
 }
