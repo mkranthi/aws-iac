c6c7e4dbef67e0d389f14ebc76c1d53726c460a0
mkranthi <kranthikumar1210@gmail.com>
2024-12-07 23:38:16 +0530

    rm s

--- modules/kms/outputs.tf
+++ modules/kms/outputs.tf
@@ -2,7 +2,6 @@ output "kms_key_arn" {
   value = aws_kms_key.dev_kms_key.arn
   description = "The ARN of the KMS key"
 }
-
 output "kms_key_id" {
   value = aws_kms_key.dev_kms_key.key_id
   description = "The ID of the KMS key"
