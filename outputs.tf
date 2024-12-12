17a31b86cead7aff7ec290fa313caaf6b71c21fb
mkranthi <kranthikumar1210@gmail.com>
2024-12-12 12:55:27 +0530

    Revert "updated outputs.tf file"  i want to remove all outputs.tf file entry This reverts commit 692cb9af16b7c270f58e9f29195a037a01086dde.

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,3 @@
 output "kms_key_arn" {
   value = module.kms.kms_key_arn
 }
-output "iam_instance_profile_name" {
-  description = "The name of the IAM instance profile"
-  value       = aws_iam_instance_profile.my_instance_profile.name
-}
-
-output "role_name" {
-  description = "The name of the primary IAM role"
-  value       = aws_iam_role.iam_role.name
-}
-
-output "kms_role" {
-  description = "The name of the KMS IAM role"
-  value       = aws_iam_role.kms.name
-}
-output "instance_id" {
-  value = aws_instance.terraform_instance.id
-  description = "The ID of the EC2 instance"
-}
