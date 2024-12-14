227db03a71cdea2679f6ba54bb66f06a9f1165cd
mkranthi <kranthikumar1210@gmail.com>
2024-12-14 19:01:32 +0530

    chnage module file

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -45,7 +45,7 @@ resource "aws_instance" "terraform_instance" {
 resource "aws_ebs_volume" "ebs_volume" {
   availability_zone = var.avzone
   size              = var.v_size
-  kms_key_id        = module.kms.kms_key_arn
+  kms_key_id        = var.kms_key_arn
   encrypted         = true
   tags = {
     name = var.volumename
