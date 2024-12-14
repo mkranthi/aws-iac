c3251147c46a9fa641e251cf7aac6c8b2a043426
mkranthi <kranthikumar1210@gmail.com>
2024-12-14 18:33:21 +0530

    delete duplicate entries

--- modules/kms/outputs.tf
+++ modules/kms/outputs.tf
@@ -1,8 +1,3 @@
-output "kms_key_arn" {
-  description = "The ARN of the KMS key"
-  value       = aws_kms_key.dev_kms_key.arn
-}
-
 output "kms_key_id" {
   description = "The ID of the KMS key"
   value       = aws_kms_key.dev_kms_key.key_id
@@ -10,5 +5,30 @@ output "kms_key_id" {
 
 output "kms_key_alias" {
   description = "The alias of the KMS key"
-  value       = aws_kms_key.dev_kms_key.key_usage
+  value       = aws_kms_key.dev_kms_key.alias
+}
+
+output "kms_key_arn" {
+  description = "The ARN of the KMS key"
+  value       = aws_kms_key.dev_kms_key.arn
+}
+
+output "kms_key_creation_date" {
+  description = "The creation date of the KMS key"
+  value       = aws_kms_key.dev_kms_key.creation_date
+}
+
+output "kms_key_description" {
+  description = "The description of the KMS key"
+  value       = aws_kms_key.dev_kms_key.description
+}
+
+output "kms_key_deletion_window_in_days" {
+  description = "The deletion window for the KMS key"
+  value       = aws_kms_key.dev_kms_key.deletion_window_in_days
+}
+
+output "kms_key_enable_key_rotation" {
+  description = "Whether key rotation is enabled for the KMS key"
+  value       = aws_kms_key.dev_kms_key.enable_key_rotation
 }
