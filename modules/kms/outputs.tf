705c14753041e29f14c0658da1c075c1de177172
mkranthi <kranthikumar1210@gmail.com>
2024-12-28 12:47:02 +0530

    added output

--- modules/kms/outputs.tf
+++ modules/kms/outputs.tf
@@ -1,7 +1,3 @@
-output "kms_key_enable_key_rotation" {
-  description = "Whether key rotation is enabled for the KMS key"
-  value       = aws_kms_key.dev_kms_key.enable_key_rotation
-}
 output "ebs_kms_key_arn" {
   description = "ARN of the EBS KMS key"
   value       = aws_kms_key.ebs_kms_key.arn
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
-output "kms_key_arn" {
-  value = module.kms.kms_key_arn
+output "ebs_kms_key_arn" {
+  description = "ARN of the EBS KMS key"
+  value       = aws_kms_key.ebs_kms_key.arn
 }
