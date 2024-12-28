26f3573bc52e656aa547f2a9cffe90e0435e2fe7
mkranthi <kranthikumar1210@gmail.com>
2024-12-28 15:01:52 +0530

    remove few outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,8 +2,3 @@ output "kms_key_arn" {
   description = "ARN of the EBS KMS key"
   value       = module.ebs_kms.kms_key_arn
 }
-
-output "ec2_instance_public_ip" {
-  description = "ARN of the EBS KMS key"
-  value       = module.ec2.aws_instance.terraform_instance.public_ip
-}
