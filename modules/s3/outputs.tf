adc8bfc0ddffcc5d2e565c874521a35b17193679
mkranthi <kranthikumar1210@gmail.com>
2024-12-29 13:47:14 +0530

    as

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -2,3 +2,7 @@ output "bucket_name" {
   description = "The name of the primary IAM role"
   value       = aws_s3_bucket.mybucket
 }
+output "s3_bucket_arn" {
+  description = "The ARN of the S3 bucket"
+  value       = aws_s3_bucket.mybucket.arn
+}
