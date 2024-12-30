1208edcf619fe7a235adc4f9e479a14d665fbf43
mkranthi <kranthikumar1210@gmail.com>
2024-12-30 16:26:53 +0530

    added allow statement

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,25 +1,32 @@
-resource "aws_s3_bucket" "mybucket" {
-  bucket = var.bucket_name
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
-  bucket = aws_s3_bucket.mybucket.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      kms_master_key_id = var.s3_kms_key_arn
-      sse_algorithm     = "aws:kms"
-    }
-  }
-}
-
 resource "aws_s3_bucket_policy" "mybucket_policy" {
-  bucket = aws_s3_bucket.mybucket.id
+  bucket = aws_s3_bucket.mybucket.bucket
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
-      # Deny all other users and roles from accessing the bucket
+      # Allow all users and roles to access
+      {
+        Sid       = "AllowSpecificRoleOrUser"
+        Effect    = "Allow"
+        Principal = {
+          AWS = [
+            "arn:aws:iam::${var.aws_account_id}:role/${var.admin_role_name}",
+           "arn:aws:iam::${var.aws_account_id}:user/${var.admin_user_name}",
+           "arn:aws:iam::${var.aws_account_id}:user/${var.admin_role_name}"
+          ]
+        },
+        Action    = [
+          "s3:ListBucket",
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ]
+        Resource = [
+          "arn:aws:s3:::${aws_s3_bucket.mybucket.bucket}",
+          "arn:aws:s3:::${aws_s3_bucket.mybucket.bucket}/*"
+        ]
+      },
+      # Deny all other users from accessing the bucket
       {
         Sid       = "DenyOtherUsers"
         Effect    = "Deny"
@@ -31,8 +38,8 @@ resource "aws_s3_bucket_policy" "mybucket_policy" {
           "s3:DeleteObject"
         ]
         Resource = [
-          "arn:aws:s3:::${var.bucket_name}",
-          "arn:aws:s3:::${var.bucket_name}/*"
+          "arn:aws:s3:::${aws_s3_bucket.mybucket.bucket}",
+          "arn:aws:s3:::${aws_s3_bucket.mybucket.bucket}/*"
         ]
         Condition = {
           StringNotEquals = {
