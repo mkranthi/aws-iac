e7f9f2b5641b694c53e9ddd899a08e8549ae9c8c
mkranthi <kranthikumar1210@gmail.com>
2024-12-30 14:09:12 +0530

    added ssh permission also

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -21,6 +21,13 @@ resource "aws_security_group" "terraform-sg" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
   ingress {
+    description      = "Allow ssh"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
     description      = "Allow HTTP 8080"
     from_port        = 8080
     to_port          = 8080
