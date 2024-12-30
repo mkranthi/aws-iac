38663f91e9552a80fc7faba37af6d40c3872aa1e
mkranthi <kranthikumar1210@gmail.com>
2024-12-30 17:53:37 +0530

    aa

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -22,8 +22,8 @@ resource "aws_s3_bucket_policy" "mybucket_policy" {
           "s3:DeleteObject"
         ]
         Resource = [
-          "arn:aws:s3:::${aws_s3_bucket.mybucket.bucket.id}",
-          "arn:aws:s3:::${aws_s3_bucket.mybucket.bucket.id}/*"
+          "arn:aws:s3:::${var.bucket_name}",
+          "arn:aws:s3:::${var.bucket_name}/*"
         ]
       },
       # Deny all other users from accessing the bucket
@@ -38,8 +38,8 @@ resource "aws_s3_bucket_policy" "mybucket_policy" {
           "s3:DeleteObject"
         ]
         Resource = [
-          "arn:aws:s3:::${aws_s3_bucket.mybucket.bucket.id}",
-          "arn:aws:s3:::${aws_s3_bucket.mybucket.bucket.id}/*"
+          "arn:aws:s3:::${var.bucket_name}",
+          "arn:aws:s3:::${var.bucket_name}/*"
         ]
         Condition = {
           StringNotEquals = {
