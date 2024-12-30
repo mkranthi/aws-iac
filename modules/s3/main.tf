74aeee1584db10bfff4f60cb2c5793a55900af03
mkranthi <kranthikumar1210@gmail.com>
2024-12-30 18:13:00 +0530

    a

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,12 +1,27 @@
+resource "aws_s3_bucket" "mybucket" {
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = var.s3_kms_key_arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
 resource "aws_s3_bucket_policy" "mybucket_policy" {
   bucket = aws_s3_bucket.mybucket.id
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
-      # Allow specific roles and users to access the bucket
+      # Allow specific IAM role or user to access the bucket and objects
       {
-        Sid       = "AllowSpecificUsers"
+        Sid       = "AllowSpecificRoleOrUser"
         Effect    = "Allow"
         Principal = {
           AWS = [
@@ -16,17 +31,29 @@ resource "aws_s3_bucket_policy" "mybucket_policy" {
           ]
         }
         Action    = [
-          "s3:ListBucket",
+          "s3:ListBucket"
+        ]
+        Resource  = "arn:aws:s3:::${aws_s3_bucket.mybucket.bucket}"
+      },
+      {
+        Sid       = "AllowObjectAccess"
+        Effect    = "Allow"
+        Principal = {
+          AWS = [
+            "arn:aws:iam::${var.aws_account_id}:role/${var.iam_role_name}",
+            "arn:aws:iam::${var.aws_account_id}:user/${var.admin_user_name}",
+            "arn:aws:iam::${var.aws_account_id}:user/${var.admin_role_name}"
+          ]
+        }
+        Action    = [
           "s3:GetObject",
           "s3:PutObject",
           "s3:DeleteObject"
         ]
-        Resource = [
-          "arn:aws:s3:::${var.bucket_name}",
-          "arn:aws:s3:::${var.bucket_name}/*"
-        ]
+        Resource  = "arn:aws:s3:::${aws_s3_bucket.mybucket.bucket}/*"
       },
-      # Deny access to everyone else
+
+      # Deny all other users and roles from accessing the bucket
       {
         Sid       = "DenyOtherUsers"
         Effect    = "Deny"
@@ -38,8 +65,8 @@ resource "aws_s3_bucket_policy" "mybucket_policy" {
           "s3:DeleteObject"
         ]
         Resource = [
-          "arn:aws:s3:::${var.bucket_name}",
-          "arn:aws:s3:::${var.bucket_name}/*"
+          "arn:aws:s3:::${aws_s3_bucket.mybucket.bucket}",
+          "arn:aws:s3:::${aws_s3_bucket.mybucket.bucket}/*"
         ]
         Condition = {
           StringNotEquals = {
