1708910576fcb3bd571b06d6b45fe2606917e93e
mkranthi <kranthikumar1210@gmail.com>
2024-12-30 10:39:23 +0530

    added deny condition to s3 bucket policy

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -19,11 +19,14 @@ resource "aws_s3_bucket_policy" "mybucket_policy" {
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
+      # Allow specific IAM role or user to access the bucket and objects
       {
-        Sid       = "BucketAccess"
+        Sid       = "AllowSpecificRoleOrUser"
         Effect    = "Allow"
         Principal = {
-          AWS = "arn:aws:iam::${var.aws_account_id}:role/${var.iam_role_name}"
+          AWS = [
+            "arn:aws:iam::${var.aws_account_id}:role/${var.iam_role_name}"
+          ]
         }
         Action    = [
           "s3:ListBucket"
@@ -31,17 +34,44 @@ resource "aws_s3_bucket_policy" "mybucket_policy" {
         Resource  = "arn:aws:s3:::${aws_s3_bucket.mybucket.bucket}"
       },
       {
-        Sid       = "AllowObjectActions"
+        Sid       = "AllowObjectAccess"
         Effect    = "Allow"
         Principal = {
-          AWS = "arn:aws:iam::${var.aws_account_id}:role/${var.iam_role_name}"
+          AWS = [
+            "arn:aws:iam::${var.aws_account_id}:role/${var.iam_role_name}"
+          ]
         }
         Action    = [
-          "s3:PutObject",
           "s3:GetObject",
+          "s3:PutObject",
           "s3:DeleteObject"
         ]
         Resource  = "arn:aws:s3:::${aws_s3_bucket.mybucket.bucket}/*"
+      },
+
+      # Deny all other users and roles from accessing the bucket
+      {
+        Sid       = "DenyOtherUsers"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = [
+          "s3:ListBucket",
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ]
+        Resource = [
+          "arn:aws:s3:::${aws_s3_bucket.mybucket.bucket}",
+          "arn:aws:s3:::${aws_s3_bucket.mybucket.bucket}/*"
+        ]
+        Condition = {
+          StringNotEquals = {
+            "aws:PrincipalArn": [
+              "arn:aws:iam::${var.aws_account_id}:role/${var.iam_role_name}",
+              "arn:aws:iam::${var.aws_account_id}:user/${var.admin_user_name}"
+            ]
+          }
+        }
       }
     ]
   })
